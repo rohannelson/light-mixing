673de654f31d005e924e269a23d8abae6e116837
Rohan Nelson <rohan.nelson@gmail.com>
2025-11-22 12:57:40 +0800

    Initial attempt at diffraction spikes to indicate colours are tertiary (and make them easier to see).

--- src/components/Block.tsx
+++ src/components/Block.tsx
@@ -1,3 +1,4 @@
+import type React from "react";
 import { toHexStr } from "../lib/utils";
 
 export default function Block({
@@ -26,6 +27,13 @@ export default function Block({
     shadow = "shadow-glow";
   }
 
+  const starry = `absolute left-1/2 top-1/2
+    w-[160%] h-[160%] -translate-x-1/2 -translate-y-1/2
+    pointer-events-none blur-[2px] contrast-[1.2]
+    [background:repeating-conic-gradient(from_0deg,rgba(var(--goal-r),var(--goal-g),var(--goal-b),0.98)_0deg_3deg,rgba(255,255,255,0)_0deg_45deg)]
+    [mask-image:radial-gradient(circle_at_center,black_30%,transparent_70%)] z-[-1]
+  `;
+
   return (
     <button
       onClick={handleClick}
@@ -34,9 +42,18 @@ export default function Block({
         handleDrop(e);
       }}
       onDragOver={(e) => e.preventDefault()}
-      className={`w-${size} bg-[${toHexStr(background)}] aspect-square ${
+      className={`w-${size} relative bg-[${toHexStr(background)}] aspect-square ${
         sandbox ? "" : "rounded-full"
       } ${shadow} shadow-[${toHexStr(goal)}]`}
-    ></button>
+      style={
+        {
+          "--goal-r": (goal >> 16) & 255,
+          "--goal-g": (goal >> 8) & 255,
+          "--goal-b": goal & 255,
+        } as React.CSSProperties
+      }
+    >
+      <span className={goal.toString(16).includes("8") ? starry : ""}></span>
+    </button>
   );
 }
